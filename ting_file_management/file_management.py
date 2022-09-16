2f321337cd415c304432ee168ad275cf1f6f53c2
Rafael Santos <rafaelalonsobarbosa@gmail.com>
2022-09-16 11:03:19 -0300

    feat(Req02): Implementada o módulo file_management

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,12 @@
+import sys
+import os
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not os.path.exists(path_file):
+        return sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+    if not path_file.endswith(".txt"):
+        return sys.stderr.write("Formato inválido\n")
+
+    with open(path_file) as file:
+        return file.read().split("\n")
