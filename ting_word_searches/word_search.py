2847b764a3e70e5636ae522bdbf5a0476d78108f
Rafael Santos <rafaelalonsobarbosa@gmail.com>
2022-09-16 13:41:46 -0300

    feat(Req04): implementado módulo word_search

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,21 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    list_words = []
+    ocorrencies = []
+
+    for item in range(len(instance.items)):
+        for i, line in enumerate(instance.search(item)["linhas_do_arquivo"]):
+            if word.lower() in line.lower():
+                ocorrencies.append({"linha": i + 1})
+
+        file_name = instance.search(item)["nome_do_arquivo"]
+        if len(ocorrencies) != 0:
+            list_words.append({
+                "palavra": word,
+                "arquivo": file_name,
+                "ocorrencias": ocorrencies,
+            })
+    return list_words
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    pass
